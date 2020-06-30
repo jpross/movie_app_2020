dffbb40ada3681e5821bb46bae2dd0c378c7cc03
jpross <ijpross@gmail.com>
2020-06-30 14:21:55 +0900

    Updates

---
+++ precache-manifest.7d3a725c9fd11873a1c0833183d6c828.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e982bd39c3bc0ca483868eb394428ec1",
+    "url": "/jpross/movie_app_2020.git/index.html"
+  },
+  {
+    "revision": "afd19930ad0864dfe962",
+    "url": "/jpross/movie_app_2020.git/static/css/main.b4892c70.chunk.css"
+  },
+  {
+    "revision": "bb38ca0ac903b612bc15",
+    "url": "/jpross/movie_app_2020.git/static/js/2.cdc3c92b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jpross/movie_app_2020.git/static/js/2.cdc3c92b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afd19930ad0864dfe962",
+    "url": "/jpross/movie_app_2020.git/static/js/main.e4587038.chunk.js"
+  },
+  {
+    "revision": "4ea3e76588d3b3d0e5e1",
+    "url": "/jpross/movie_app_2020.git/static/js/runtime-main.bbdb94f4.js"
+  }
+]);
